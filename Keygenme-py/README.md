0273940292a2e7052bfde49d752184acbbeddc3c
Michael <michaelarnold615@gmail.com>
2022-03-23 16:14:07 -0700

    Challenge 13

--- Keygenme-py/README.md
+++ Keygenme-py/README.md
@@ -1,7 +1,19 @@
 # Keygenme-py
 
-## FLAG: picoCTF{}
+## FLAG: picoCTF{1n_7h3_|<3y_of_e584b363}
 
 Category: Reverse-Engineering
 
 Description: keygenme-trial.py
+
+>wget "https://mercury.picoctf.net/static/9cc50abd5b012891d5a1132e05f15a07/keygenme-trial.py"
+
+First part of key: picoCTF{1n_7h3_|<3y_of_
+Second part of key: xxxxxxxx
+Last part of key: }
+***Second part of key is 8 characters long***
+
+Ran the hashlib.sha256(username_trial).hexdigest()\[X\] that generates the middle 8 characters
+Middle 8: e584b363
+
+Ran flag.py to join all parts together to create flag
---
+++ Keygenme-py/flag.py
@@ -0,0 +1,22 @@
+import hashlib
+
+username_trial = "SCHOFIELD"
+bUsername_trial = b"SCHOFIELD"
+key_part1 = "picoCTF{1n_7h3_|<3y_of_"
+key_part2 = ""
+key_part3 = "}"
+
+characters = []
+characters.append(hashlib.sha256(bUsername_trial).hexdigest()[4])
+characters.append(hashlib.sha256(bUsername_trial).hexdigest()[5])
+characters.append(hashlib.sha256(bUsername_trial).hexdigest()[3])
+characters.append(hashlib.sha256(bUsername_trial).hexdigest()[6])
+characters.append(hashlib.sha256(bUsername_trial).hexdigest()[2])
+characters.append(hashlib.sha256(bUsername_trial).hexdigest()[7])
+characters.append(hashlib.sha256(bUsername_trial).hexdigest()[1])
+characters.append(hashlib.sha256(bUsername_trial).hexdigest()[8])
+
+key_part2 = ''.join(characters)
+
+flag = key_part1 + key_part2 + key_part3
+print(flag)
